12a17345efd69fa7689e09fc13af685cf0aa2886
Nada_Nasser <36134746+Nada-Nasser@users.noreply.github.com>
2020-08-22 20:11:02 +0200

    Build Project Structure

---
+++ TwitterDemoApp/app/src/main/java/com/example/twitterdemoapp/SearchType.java
@@ -0,0 +1,12 @@
+package com.example.twitterdemoapp;
+
+
+/**
+ * Created by hussienalrubaye on 11/13/16.
+ */
+
+public class SearchType {
+    public static int MyFollowing=1;
+    public static int OnePerson=2;
+    public static int SearchIn=3;
+}
